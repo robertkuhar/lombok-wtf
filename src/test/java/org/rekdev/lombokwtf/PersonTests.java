91f32141dc96651775ff967991699ba5f7e77e19
robert.kuhar <robert.kuhar@climate.com>
2018-04-10 20:21:58 -0700

    Clean up the import

--- src/test/java/org/rekdev/lombokwtf/PersonTests.java
+++ src/test/java/org/rekdev/lombokwtf/PersonTests.java
@@ -5,24 +5,23 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class PersonTests {
   @Test
   public void PersonTestEqualsContract() throws Exception {
-    Person p0 = new Person("Robert", "Kuhar", new Date(64,6,18));
-    Person p1 = new Person("Robert","Kuhar", new Date(64,6,18));
-    Person p2 = new Person("Chloe", "Kuhar", new Date(99,5,6));
+    Person p0 = new Person("Robert", "Kuhar", new Date(64, 6, 18));
+    Person p1 = new Person("Robert", "Kuhar", new Date(64, 6, 18));
+    Person p2 = new Person("Chloe", "Kuhar", new Date(99, 5, 6));
 
-    assertEquals(p0,p1);
-    assertEquals(p0.hashCode(),p1.hashCode());
-    assertNotEquals(p1,p2);
-    assertNotEquals(p1.hashCode(),p2.hashCode());
+    assertEquals(p0, p1);
+    assertEquals(p0.hashCode(), p1.hashCode());
+    assertNotEquals(p1, p2);
+    assertNotEquals(p1.hashCode(), p2.hashCode());
   }
 
   @Test(expected = NullPointerException.class)
   public void testNonNull() throws Exception {
-    Person prince = new Person("Prince", null, new Date(58, 5,7));
+    Person prince = new Person("Prince", null, new Date(58, 5, 7));
   }
 }
