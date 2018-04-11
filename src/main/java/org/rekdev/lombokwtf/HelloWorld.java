69c14267e418a88b73546b687052e2a056c0b726
robert.kuhar <robert.kuhar@climate.com>
2018-04-11 12:19:03 -0700

    Add some Person and Card stuff to HelloWorld

--- src/main/java/org/rekdev/lombokwtf/HelloWorld.java
+++ src/main/java/org/rekdev/lombokwtf/HelloWorld.java
@@ -1,7 +1,17 @@
 package org.rekdev.lombokwtf;
 
+import java.sql.Date;
+import java.time.Instant;
+
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World.");
+    Instant july_18_1964 = Instant.parse("1964-07-18T00:00:00.000Z");
+
+    Person robertKuhar = new Person("Robert", "Kuhar", Date.from(july_18_1964));
+    System.out.printf("robertkuhar: %s\n", robertKuhar);
+
+    Card queenOfHearts = new Card(Card.Suit.HEARTS, Card.Rank.QUEEN);
+    System.out.printf("queenOfHearts: %s, isFaceCard=%s\n", queenOfHearts, queenOfHearts.getRank().isFaceCard());
   }
 }
